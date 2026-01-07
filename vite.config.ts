09c7b019de0447257acea1e70252e0f2dbb59348
nino <nino@isideweb.com>
2026-01-07 13:36:05 +0100

    Aggiunta a pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/charta-obscura-v2/', // Sostituisci con il nome esatto del tuo repository su GitHub
 })
